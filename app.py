305c438f72e9be9f41260de083e23a5a8e1af368
SeiShin32 <marko.wacker@gmail.com>
2021-09-07 23:39:33 +0300

    sqlite-ver1

--- app.py
+++ app.py
@@ -89,12 +89,6 @@ def delete_link():
     return redirect("/")
 
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
 
-@app.route("/signup")
-def signup():
-    return render_template('signup.html')
 
 
